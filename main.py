4f2c38a5bcc8068623c506979c05d5471973991f
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-26 01:55:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ numero2 = eval(input("Introduzca el segundo número: "))
 
 # Proceso
 if numero1 % numero2 == 0:
-	resultado = "El número " + str(numero1) + " es múltiplo del número " \
+	resultado = "El número " + str(numero1) + " es múltiplo del " \
         + str(numero2) + "."
 elif numero2 % numero1 == 0:
-    resultado = "El número " + str(numero2) + " es múltiplo del número " \
+    resultado = "El número " + str(numero2) + " es múltiplo del " \
         + str(numero1) + "."
 else:
 	resultado = "Ninguno de los números es múltiplo del otro."
