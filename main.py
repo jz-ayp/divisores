82603355ca8dd4fee8c256aba2be7d8919db802d
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-26 02:02:45 -0600

    Update main.py
    Delete solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-
+"""
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,20 +13,3 @@ entrada = input()
 
 # Salidas
 print(salida)
-"""
-# Entradas
-numero1 = eval(input("Introduzca el primer número: "))
-numero2 = eval(input("Introduzca el segundo número: "))
-
-# Proceso
-if numero1 % numero2 == 0:
-	resultado = "El número " + str(numero1) + " es múltiplo del " \
-        + str(numero2) + "."
-elif numero2 % numero1 == 0:
-    resultado = "El número " + str(numero2) + " es múltiplo del " \
-        + str(numero1) + "."
-else:
-	resultado = "Ninguno de los números es múltiplo del otro."
-
-# Salidas
-print(resultado)
