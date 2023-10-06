3a764a79cca52601a16fe366287107bc6307fb88
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-06 01:18:18 -0600

    Add solution

--- divisores.py
+++ divisores.py
@@ -1,15 +1,20 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Dados dos números, determinar si uno es divisor del otro
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero1 = eval(input("Introduzca el primer número: "))
+numero2 = eval(input("Introduzca el segundo número: "))
 
 # Proceso
-
+if numero1 % numero2 == 0:
+	resultado = "El número " + str(numero2) + " es divisor del número " \
+        + str(numero1) + "."
+elif numero2 % numero1 == 0:
+    resultado = "El número " + str(numero1) + " es divisor del número " \
+        + str(numero2) + "."
+else:
+	resultado = "Ninguno de los números es divisor del otro."
 
 # Salidas
-print(salida)
+print(resultado)
