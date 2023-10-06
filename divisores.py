6a506ba48aa145fab41e410ead3fb17446ce58dc
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-06 01:23:04 -0600

    Correct wording of output

--- divisores.py
+++ divisores.py
@@ -8,11 +8,9 @@ numero2 = eval(input("Introduzca el segundo número: "))
 
 # Proceso
 if numero1 % numero2 == 0:
-	resultado = "El número " + str(numero2) + " es divisor del número " \
-        + str(numero1) + "."
+	resultado = f"El número {numero1} es múltiplo del número {numero2}."
 elif numero2 % numero1 == 0:
-    resultado = "El número " + str(numero1) + " es divisor del número " \
-        + str(numero2) + "."
+    resultado = f"El número {numero2} es múltiplo del número {numero1}."
 else:
 	resultado = "Ninguno de los números es divisor del otro."
 
